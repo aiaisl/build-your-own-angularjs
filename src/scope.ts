3c39fb73a700482cb28febbccb557ef667238183
aiaisl <aiaisl@outlook.com>
2016-10-09 19:57:29 +0800

    单元测试

--- src/scope.ts
+++ src/scope.ts
@@ -1,3 +1,21 @@
+interface Warcher {
+    watchFn: Function;
+    listenerFn: Function;
+}
 class Scope{
-    
+    $$watchers = [];
+    $watch(watchFn, listenerFn) {
+        let watcher = {
+            watchFn: watchFn,
+            listenerFn: listenerFn
+        };
+
+        this.$$watchers.push(watcher);
+    }
+
+    $digest() {
+        _.forEach(this.$$watchers, (watcher)=>{
+            watcher.listenerFn();
+        })
+    }
 }
--- test/scope_spec.ts
+++ test/scope_spec.ts
@@ -5,3 +5,25 @@ describe('Scope', ()=> {
         expect(scope.aProperty).toBe(1);
     })
 })
+
+describe('digest', ()=> {
+    let scope: Scope;
+    beforeEach(()=>{
+        scope = new Scope();
+    });
+
+    it('calls the listener function of a watch on first $digest', ()=> {
+        let watchFn = ()=> {
+            return 'wat';
+        }
+
+        let listenerFn = jasmine.createSpy('lsadfistenerFn');
+
+        scope.$watch(watchFn, listenerFn);
+
+        scope.$digest();
+
+        expect(listenerFn).toHaveBeenCalled();
+
+    })
+})
